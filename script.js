cfacbbfae39def8846ca8257e0a9d40bbf733aaf
El <105391840+El-404@users.noreply.github.com>
2023-05-09 13:24:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+var plr = document.getElementById("player");
 
+const var speed = 10;
+
+document.addEventListener("keyPressed", (e) => {
+  if(e.code == "w"){
+    plr.y += speed
+  }else if(e.code == "s"){
+    plr.y -= speed;
+  }else if(e.code == "a"){
+    plr.x -= speed;
+  }else if(e.code == "d"){
+    plr.x += speed;
+  }
+});
