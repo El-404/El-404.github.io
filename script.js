17994031b00286a27b9082f288def9697f2d99e9
El <105391840+El-404@users.noreply.github.com>
2023-05-09 13:27:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const var speed = 10;
 
 document.addEventListener("keyPressed", (e) => {
   if(e.code == "w"){
-    plr.y += speed
+    plr.top -= speed
   }else if(e.code == "s"){
-    plr.y -= speed;
+    plr.top += speed;
   }else if(e.code == "a"){
-    plr.x -= speed;
+    plr.left -= speed;
   }else if(e.code == "d"){
-    plr.x += speed;
+    plr.left += speed;
   }
 });
