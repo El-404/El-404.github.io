0a7a0fee881000923e8afab6a28e87e68c98a6ff
Pengiun26 <awesomepenguins54321@gmail.com>
2024-05-21 23:49:14 -0500

    updated

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let objs = document.getElementsByClassName("navButton");
+let buttons = document.getElementsByClassName("navButton");
+let imgs = document.getElementsByClassName("img");
 
-for (let i = 0; i < objs.length; i++) {
-  console.log("A");
-  objs.item(i).onclick = function() {
+for(let i = 0; i < buttons.length; i++) {
+  buttons.item(i).onclick = function() {
     window.location.href = this.getAttribute("data-link");
   };
+};
+
+for(let i = 0; i < imgs.length; i++) {
+  imgs.item(i).onclick = function() {
+    let p = document.getElementById(this.getAttribute("data-txtId"));
+    if(p.style.display == "none") p.style.display = "block";
+    else p.style.display = "none";
+  }
 }
